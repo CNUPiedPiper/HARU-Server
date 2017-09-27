fe0a1a1261470dfc4b4838d9fa41377adf3e8bf4
SuminJegal <geniusry@naver.com>
2017-09-27 13:04:21 +0900

    Upload requirement.txt and update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+
+echo y | command
+
+pip install -r requirements.txt
+pip install --upgrade gensim
+
 geo_data="./src/GeoLiteCity.dat"
 if [ -f "$geo_data" ]
 then
     echo "$geo_data already exist."
 else
     cd src
-    #wget -M http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+    wget -M http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
     curl -O http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
     gzip -d GeoLiteCity.dat.gz
     cd ..
