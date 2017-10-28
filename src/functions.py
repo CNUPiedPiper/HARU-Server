e0d062dea582f1f0e78a6357e538e423300ded45
yoon-jae <yoonjae.cho92@gmail.com>
2017-10-28 19:55:28 +0900

    Implement evaluation unittest

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 from apibucket import weather, issue, geoip, wiki, date, word_play
-from apibucket.music_recognizer import music_recog
+#from apibucket.music_recognizer import music_recog
 from sentence2vec import word2vec
 import datetime
 import configparser
@@ -69,10 +69,13 @@ def function3(words):
     return u'지금은 {h}시 {m}분 입니다.'.format(h=now.hour, m=now.minute)
 
 def function4(words):
+    '''
 	host = config.get('MUSIC_RECOGNIZER', 'host')
 	key = config.get('MUSIC_RECOGNIZER', 'key')
 	secret = config.get('MUSIC_RECOGNIZER', 'secret')
 	return music_recog.get_music_title(host, key, secret)
+    '''
+    return u"노래를 들려주세요."
 
 def function5(words):
     title = [re.findall(r"[가-힣\w]+", word)[0] for word in words if re.findall(r"[가-힣\w]+", word)[1] == u'Noun'][0]
